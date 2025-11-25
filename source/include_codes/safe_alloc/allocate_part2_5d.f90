06260415d4d7280d55af6ac45f16e74b1d2335b6
Anders Bergman <anders.bergman@physics.uu.se>
2025-11-25 08:55:09 +0100

    Added missing safe_alloc source

---
+++ source/include_codes/safe_alloc/allocate_part2_5d.f90
@@ -0,0 +1,6 @@
+integer, dimension(5), intent(in) :: list_size
+if (allocated(list)) then
+   deallocate (list)
+   call g_logger%warning('Reallocating list "'//trim(label)//'" of size '//int2str(size(list))//' to new size '//int2str(list_size(1))//', '//int2str(list_size(2))//', '//int2str(list_size(3))//', '//int2str(list_size(4))//', '//int2str(list_size(5)), 'safe_alloc.f90', -1)
+end if
+allocate (list(list_size(1), list_size(2), list_size(3), list_size(4), list_size(5)))
